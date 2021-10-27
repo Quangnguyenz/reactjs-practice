4c754f3c3b51d6bc0804524c84febf079cf42ee5
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-10-26 20:13:44 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function Greeting() {
-  return <h4>a component</h4>
+function BookList() {
+  return (<section>
+    <Book />
+  </section>)
+
 }
 
-ReactDom.render(<Greeting />, document.getElementById('root'))
+const Book = () => {
+  return <article>
+    <Image />
+    <Title />
+    <Author />
+  </article>
+}
+
+
+
+const Image = () => <img src="https://media.istockphoto.com/photos/school-picture-id1016131800?b=1&k=20&m=1016131800&s=170667a&w=0&h=imifkJTvS-4b8cvOhVNlCqqvZlwI4S-xCKZ6Oxbla4Q=" alt="" />
+
+const Title = () => <h1>A title of the book 5</h1>
+
+const Author = () => <h1>Author name</h1>
+
+ReactDom.render(<BookList />, document.getElementById('root'))
