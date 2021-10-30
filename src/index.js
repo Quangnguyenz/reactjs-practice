673409045db56747844378481c3191f0e7953ad6
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-10-30 18:28:03 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -2,23 +2,43 @@ import React from 'react';
 import ReactDom from 'react-dom';
 // CSS 
 import './index.css';
+// set up vars 
+const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/71BTth-z7iL._AC_UL127_SR127,127_.jpg",
+    title: 'A demo book 1',
+    author: 'Jun Yuu',
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/61I1N9jpTGL._AC_UL127_SR127,127_.jpg",
+    title: 'A demo book 2',
+    author: 'Chang Yin',
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/61I1N9jpTGL._AC_UL127_SR127,127_.jpg",
+    title: 'A demo book 3',
+    author: 'Lapag',
+  }]
 
 function BookList() {
   return (<section className="booklist">
-    <Book />
-
+    {books.map((book) => {
+      const { img, title, author } = book
+      return (
+        <Book book={book}></Book>
+      )
+    })}
   </section>);
 
 }
 
-const Author = 'Author name';
-const Book = () => {
-  const title = 'A title of the book 5sdsd';
+const Book = (props) => {
+  const { img, title, author } = props.book
+
   return <article className="book">
-    <img src="https://images-na.ssl-images-amazon.com/images/I/71BTth-z7iL._AC_UL127_SR127,127_.jpg" alt="" />
+    <img src={img} alt="" />
     <h1>{title}</h1>
-    <h4 >{Author.toUpperCase()}</h4>
-    <p>{6 + 6}</p>
+    <h4 >{author}</h4>
   </article>
 }
 
