746a0f31d4aca45d83fd5dfc953c38c47a5ce9fb
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-10-29 20:45:56 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -6,29 +6,20 @@ import './index.css';
 function BookList() {
   return (<section className="booklist">
     <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
+
   </section>);
 
 }
 
+const Author = 'Author name';
 const Book = () => {
+  const title = 'A title of the book 5sdsd';
   return <article className="book">
-    <Image />
-    <Title />
-    <Author />
+    <img src="https://images-na.ssl-images-amazon.com/images/I/71BTth-z7iL._AC_UL127_SR127,127_.jpg" alt="" />
+    <h1>{title}</h1>
+    <h4 >{Author.toUpperCase()}</h4>
+    <p>{6 + 6}</p>
   </article>
 }
-
-
-
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/71BTth-z7iL._AC_UL127_SR127,127_.jpg" alt="" />
-
-const Title = () => <h1>A title of the book 5</h1>
-
-const Author = () => <h4 style={{ color: '#618d98', fontSize: "0.75rem", marginTop: '0.25rem' }}>Author name</h4>
 
 ReactDom.render(<BookList />, document.getElementById('root'))
