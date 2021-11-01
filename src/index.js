bc3509f3d48bdd6a8bd8f0947173397c7bd2a0be
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-10-31 19:38:34 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import './index.css';
 // set up vars 
 const books = [
   {
+    id: 1,
     img: "https://images-na.ssl-images-amazon.com/images/I/71BTth-z7iL._AC_UL127_SR127,127_.jpg",
     title: 'A demo book 1',
     author: 'Jun Yuu',
   },
   {
+    id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/I/61I1N9jpTGL._AC_UL127_SR127,127_.jpg",
     title: 'A demo book 2',
     author: 'Chang Yin',
   },
   {
+    id: 3,
     img: "https://images-na.ssl-images-amazon.com/images/I/61I1N9jpTGL._AC_UL127_SR127,127_.jpg",
     title: 'A demo book 3',
     author: 'Lapag',
@@ -23,22 +26,34 @@ const books = [
 function BookList() {
   return (<section className="booklist">
     {books.map((book) => {
-      const { img, title, author } = book
       return (
-        <Book book={book}></Book>
+        <Book key={book.id} {...book}></Book>
       )
     })}
   </section>);
 
 }
 
-const Book = (props) => {
-  const { img, title, author } = props.book
+const Book = ({ img, title, author }) => {
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = (e) => {
+    alert("Hello")
+    console.log(e.target)
+  }
 
-  return <article className="book">
+  const complexExample = (author) => {
+    console.log(author)
+  }
+
+  return <article className="book" onMouseOver={() => {
+    console.log(title)
+  }}>
     <img src={img} alt="" />
-    <h1>{title}</h1>
+    <h1 onClick={() => { console.log(title) }}>{title}</h1>
     <h4 >{author}</h4>
+    <button type="button" onClick={clickHandler}>A button</button>
+    <button type="button" onClick={() => complexExample(author)}>More complex example</button>
   </article>
 }
 
